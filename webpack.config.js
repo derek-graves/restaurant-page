643ead41eb2a84d24df541d8b076600112dc7142
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2023-02-26 08:55:58 -0500

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
