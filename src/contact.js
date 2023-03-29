ba975ef0ea41cf72e7c1a3937e7821d3ac10771c
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2023-03-29 14:40:51 -0400

    Add module for contact content

---
+++ src/contact.js
@@ -0,0 +1,9 @@
+const contact = () => {
+  // maintain or adjust .main bg-color
+  const main = document.querySelector(".main");
+  if (!main.classList.contains("show-main-bg")) {
+    main.classList.add("show-main-bg");
+  }
+
+  // generate contents
+};
